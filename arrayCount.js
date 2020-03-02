8c7daa3b11700dddc0db965e27a3ca24a557cef7
96wilkinson <58259270+96wilkinson@users.noreply.github.com>
2020-03-02 10:25:34 +0000

    Add files via upload

---
+++ arrayCount.js
@@ -0,0 +1,4 @@
+let numbers = [1,23,3,76,52,45,-3,6,7];
+numbers = numbers.sort();
+console.log("smallest number is: "+ numbers[0]);
+console.log("Largest Number is: " + numbers[numbers.length - 1]);
